3927c59d0a40cc0dd3fe07551ffc31adf0cae877
madpsyco <arjunaravinda@gmail.com>
2023-08-11 16:36:23 +0530

    added

---
+++ guruvi/guruviapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class Course(models.Model):
+    course = models.CharField(max_length=250)
+    posted = models.DateField()
+    courseimage = models.ImageField(upload_to="pics/",default="")
+    category = models.TextField()
+
+    def __str__(self):
+        return self.course
+
+
